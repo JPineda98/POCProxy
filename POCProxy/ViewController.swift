3acf8a56b7a9bc32b2ab1430fab8f257f772f1d5
Javier Pineda <jpgpineda98@gmail.com>
2022-06-23 14:00:30 -0500

    Initial Commit

---
+++ POCProxy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCProxy
+//
+//  Created by Javier Pineda Gonzalez on 23/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
